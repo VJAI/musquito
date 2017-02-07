94985ba7bbc09905c0f6161c6ec304929bd943fe
Vijaya Anand <vanand@apttus.com>
2017-02-07 18:30:39 +0530

    Made some working code to change the volume of a buzz

--- buzzer.js
+++ buzzer.js
@@ -1,152 +1,221 @@
 (function (ctxClass) {
-  
+
   'use strict';
 
-  var emptyFn = function () {};
+  var DEBUG = 'ON', emptyFn = function () {
+  }, log = (function () {
+    return DEBUG === 'ON' ? function (message, type) {
+      console[type || 'log'](message);
+    } : emptyFn;
+  })();
 
   var BuzzerState = {
     Constructed: 0,
     Ready: 1,
     Done: 2
   };
-  
+
+  /**
+   * Buzzer
+   * @constructor
+   */
   function Buzzer() {
     this.ctx = null;
     this.muted = false;
     this.vol = 1.0;
     this.state = BuzzerState.Constructed;
   }
-  
+
   Buzzer.prototype.setup = function (context) {
-    if(this.state !== BuzzerState.Constructed) {
+    if (this.state === BuzzerState.Ready) {
       return;
     }
-    
+
     this.ctx = context || new ctxClass();
     this.gain = this.ctx.createGain();
     this.gain.gain.value = this.vol;
     this.gain.connect(this.ctx.destination);
     this.state = BuzzerState.Ready;
   };
-  
+
   Buzzer.prototype.volume = function (vol) {
     this.vol = vol;
     this.gain.value = this.vol;
     return this.gain.value;
   };
-  
+
   Buzzer.prototype.mute = function () {
-    if(this.muted) {
+    if (this.muted) {
       return;
     }
-    
+
     this.gain.gain.value = 0;
     this.muted = true;
   };
-  
+
   Buzzer.prototype.unmute = function () {
-    if(!this.muted) {
+    if (!this.muted) {
       return;
     }
-    
+
     this.gain.gain.value = this.vol;
     this.muted = false;
   };
-  
+
   Buzzer.prototype.tearDown = function () {
     this.state = BuzzerState.Done;
   };
-  
+
   Buzzer.prototype.context = function () {
     return this.ctx;
   };
-  
+
   Buzzer.prototype.isMuted = function () {
     return this.muted;
   };
-  
-  var buzzer = new Buzzer();
-  var cache = {};
-  
+
+  var buzzer = new Buzzer(), cache = {};
+
+  var BuzzState = {
+    Constructed: 0,
+    Playing: 1,
+    Paused: 2,
+    Muted: 3,
+    Stopped: 4
+  };
+
+  /**
+   * Buzz
+   * @param args
+   * @constructor
+   */
   function Buzz(args) {
     buzzer.setup();
-    
+
     this.id = Math.round(Date.now() * Math.random());
     this.src = args.src;
-    this.volume = args.volume || 1.0;
+    this.vol = args.volume || 1.0;
     this.buffer = null;
+    this.duration = 0;
+    this.endTimer = null;
     var context = buzzer.context();
     this.gain = context.createGain();
     this.gain.connect(buzzer.gain);
-    
-    this.gain.gain.value = this.volume;
+
+    this.gain.gain.value = this.vol;
     this.loaded = false;
+    this.state = BuzzState.Constructed;
   }
-  
+
   Buzz.prototype.load = function (success, error) {
-    if(this.loaded) {
-      success.call(this, this.buffer);
-      return;
+    if (this.loaded) {
+      return success.call(this, this.buffer);
     }
-    
-    if(cache.hasOwnProperty(this.src)) {
+
+    if (cache.hasOwnProperty(this.src)) {
       this.buffer = cache[this.src];
-      success.call(this, this.buffer);
-      return;
+      this.loaded = true;
+      return success.call(this, this.buffer);
     }
-    
-    var xhr = new XMLHttpRequest(), self = this;
+
+    var xhr = new XMLHttpRequest();
     xhr.open('GET', this.src, true);
     xhr.responseType = 'arraybuffer';
-    xhr.addEventListener('load', function () {
+
+    var onLoad = function () {
       buzzer.context().decodeAudioData(xhr.response, function (buffer) {
-        cache[self.src] = buffer;
-        self.buffer = buffer;
-        self.loaded = true;
-        success.call(self, self.buffer);
-      }, error);
-    });
-    xhr.addEventListener('error', error);
+        cache[this.src] = buffer;
+        this.buffer = buffer;
+        this.duration = buffer.duration;
+        this.loaded = true;
+        log(this.src + ' is loaded!');
+        success.call(this, this.buffer);
+      }.bind(this), onError);
+    }.bind(this);
+
+    var onError = function (err) {
+      log(err, 'error');
+      error && error();
+    };
+
+    xhr.addEventListener('load', onLoad);
+    xhr.addEventListener('error', onError);
     xhr.send();
   };
-  
+
   Buzz.prototype.play = function (end, error) {
     var play = function () {
+      if (this.endTimer) {
+        clearTimeout(this.endTimer);
+      }
+
       var bufferSource = buzzer.context().createBufferSource();
       bufferSource.buffer = this.buffer;
       bufferSource.connect(this.gain);
+
+      this.endTimer = setTimeout(playEnd, this.duration * 1000);
+      this.state = BuzzState.Playing;
+
       bufferSource.start(0);
     };
 
-    var err = function (e) {
-      console.log(e);
+    var playEnd = function () {
+      this.state = BuzzState.Stopped;
+      end && end();
     };
 
-    if(!this.loaded) {
-      this.load(play, error || err);
-      return;
+    var onError = function (e) {
+      log(e, 'error');
+      error && error();
+    };
+
+    if (!this.loaded) {
+      return this.load(play, onError);
     }
 
     play.call(this);
   };
-  
+
   Buzz.prototype.stop = function () {
-    
+
   };
-  
+
   Buzz.prototype.pause = function () {
-    
+
   };
-  
+
   Buzz.prototype.mute = function () {
-    
+    if(this.state === BuzzState.Muted) {
+      return;
+    }
+
+    this.gain.gain.value = 0;
+    this.state = BuzzState.Muted;
   };
-  
-  Buzz.prototype.volume = function () {
-    
+
+  Buzz.prototype.unmute = function () {
+    if(this.state !== BuzzState.Muted) {
+      return;
+    }
+
+
   };
-  
+
+  Buzz.prototype.volume = function (vol) {
+    var volume = parseFloat(vol);
+    if(isNaN(volume) || volume < 0 || volume > 1.0) {
+      return;
+    }
+
+    this.vol = volume;
+    this.gain.gain.value = this.vol;
+  };
+
+  Buzz.prototype.getState = function () {
+    return this.state;
+  };
+
   window.buzzer = buzzer;
   window.Buzz = Buzz;
-  
+
 })(window.AudioContext || window.webkitAudioContext);
--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 (function () {
-  
+
   'use strict';
-  
+
   function buzzerTester() {
-    
+
     init();
-    
+
     var buzzer = window.buzzer, Buzz = window.Buzz;
     var buzzes = [];
     var buzzerMuteBtn, buzzerVolumeSlider, buzzCreateForm, buzzesList;
-    
+
     function init() {
       buzzerMuteBtn = document.getElementById('buzzer-mute');
       buzzerMuteBtn.addEventListener('click', muteBuzzer, false);
-      
+
       buzzerVolumeSlider = document.getElementById('buzzer-volume');
       buzzerVolumeSlider.addEventListener('change', changeVolumeBuzzer, false);
-      
+
       buzzCreateForm = document.forms.buzzCreateForm;
       buzzCreateForm.addEventListener('submit', createNewBuzz, false);
-  
+
       buzzesList = document.getElementById('buzzes');
     }
-    
+
     function muteBuzzer() {
-      if(buzzer.isMuted) {
+      if (buzzer.isMuted) {
         buzzer.unmute();
       } else {
         buzzer.mute();
       }
     }
-    
+
     function changeVolumeBuzzer() {
       var volume = buzzerVolumeSlider.value;
       buzzer.volume(volume);
     }
-    
+
     function createNewBuzz(event) {
       var src = 'sounds/' + buzzCreateForm["buzz-sound"].value,
-          volume = parseFloat(buzzCreateForm["buzz-volume"].value),
-          loop = buzzCreateForm["buzz-loop"].value;
-      
+        volume = parseFloat(buzzCreateForm["buzz-volume"].value),
+        loop = buzzCreateForm["buzz-loop"].value;
+
       var buzz = new Buzz({
         src: src,
         volume: volume,
@@ -50,8 +50,8 @@
 
       var buzzLi = document.createElement('li');
       buzzLi.innerHTML = '<p><em>#sound#</em><button class="buzz-play">Play</button><button class="buzz-pause">Pause</button><button class="buzz-mute">Mute</button><button class="buzz-stop">Stop</button><input class="buzz-volume" type="range" min="0.0" max="1.0" step="0.1" value="1.0"/></p>'
-          .replace('#id#', buzz.id)
-          .replace('#sound#', src);
+        .replace('#id#', buzz.id)
+        .replace('#sound#', src);
       buzzesList.appendChild(buzzLi);
 
       var self = this;
@@ -72,9 +72,9 @@
       }, false);
 
       buzzLi.querySelector('.buzz-volume').addEventListener('change', function () {
-        changeVolumeBuzz.call(self, buzz);
+        changeVolumeBuzz.call(self, buzz, this.value);
       }, false);
-      
+
       event.preventDefault();
       return false;
     }
@@ -82,24 +82,24 @@
     function playBuzz(buzz) {
       buzz.play();
     }
-    
+
     function pauseBuzz(buzz) {
       buzz.pause();
     }
-    
+
     function muteBuzz(buzz) {
       buzz.mute();
     }
-    
+
     function stopBuzz(buzz) {
       buzz.stop();
     }
 
-    function changeVolumeBuzz(buzz) {
-      buzz.volume(this.value);
+    function changeVolumeBuzz(buzz, volume) {
+      buzz.volume(volume);
     }
   }
-  
+
   window.addEventListener('load', function () {
     buzzerTester();
   }, false);
