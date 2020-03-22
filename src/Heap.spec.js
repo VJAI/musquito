366d629c04ebe446d02699afe7a2b049e67218ed
Vijaya Anand <vanand@apttus.com>
2020-03-22 13:20:46 +0530

    Heap test started

---
+++ src/Heap.spec.js
@@ -0,0 +1,25 @@
+import Heap from './Heap';
+
+describe('Heap', () => {
+
+  let heap = null;
+
+  beforeEach(() => {
+    heap = new Heap(1);
+  });
+
+  afterEach(() => {
+    heap.destroy();
+  });
+
+  describe('on adding sound', () => {
+
+    it('should store the sound', () => {
+
+    });
+  });
+
+  describe('on calling free', () => {
+
+  });
+});
